8d9a6b6606277731f506c78068d0823bdc14f363
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-10-17 02:31:28 +0200

    unlock account by default

--- run-parity.sh
+++ run-parity.sh
@@ -2,4 +2,9 @@
 
 echo "address: 0x00a329c0648769a73afac7f9381e08fb43dbea72"
 
-/parity/parity --config config.toml --ui-no-validation
+mkdir -p /root/.local/share/io.parity.ethereum/keys/DevelopmentChain/
+cp ./keys.json /root/.local/share/io.parity.ethereum/keys/development-account
+
+echo "" >  /password.file
+
+/parity/parity --config config.toml --ui-no-validation --password "/password.file"
